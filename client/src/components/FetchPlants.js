586241c861ab70faeff074ae0d5e25d2fb4a7dbc
Brian Choi <b.choi562@gmail.com>
2020-12-04 10:50:58 -0800

    sprint challenge

---
+++ client/src/components/FetchPlants.js
@@ -0,0 +1,12 @@
+// import axios from 'axios';
+
+// const fetchPlants = () => {
+//     return axios.get(`http//localhost:3333/plants`)
+//     .then((resp)=>{
+//         console.log(resp.data);
+//         return (resp);
+//     })
+//     .catch(err=>console.log(err));
+// }
+
+// export default fetchPlants;
